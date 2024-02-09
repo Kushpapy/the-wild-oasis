060ccd09c2419f6e594bf03d402fa8bf60d7952d
kushpapy <nwankwoalabj@gmail.com>
2024-02-09 20:29:25 +0100

    updated header menu file

--- src/ui/HeaderMenu.jsx
+++ src/ui/HeaderMenu.jsx
@@ -3,7 +3,7 @@ import Logout from "../features/authentication/Logout";
 import ButtonIcon from "./ButtonIcon";
 import { HiOutlineUser } from "react-icons/hi2";
 import { useNavigate } from "react-router-dom";
-import DarkmodeToggle from "./DarkmodeToggle";
+import DarkmodeToggle from "./DarkModeToggle";
 
 const StyledHeaderMenu = styled.ul`
   display: flex;
